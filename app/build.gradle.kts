e33ef817c68c432cdd6c4b9698e1f26e0f9b0dc1
ronit-khalate <ronitkhalate243@gmail.com>
2023-12-14 16:29:16 +0530

    Added Navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Navigation
+    val nav_version = "2.7.6"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+
 }
